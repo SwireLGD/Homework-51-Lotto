bba8da52a8de2d69b35a8db63f96414f0e729f51
Arsen Chernov <arsenchernov02@gmail.com>
2023-12-22 17:51:33 +0600

    Changed app.tsx code and index.css, deleted svg files, app.css, changed title to Lotto in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Lotto from './Lotto';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const createNewNum = (): number[] => {
+    const newNum: number[] = [];
+    while (newNum.length < 5) {
+      const randomNumber = Math.floor(Math.random() * 32) + 5;
+      if (!newNum.includes(randomNumber)) {
+        newNum.push(randomNumber);
+      }
+    }
+    return newNum.sort((a, b) => a - b);
+  };
+
+  const [numbers, setNumbers] = useState<number[]>(createNewNum());
+
+  const handleNewNumbers = () => {
+    setNumbers(createNewNum());
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='ContentBox'>
+      <button className='LottoBtn' onClick={handleNewNumbers}>New numbers</button>
+      <div className='LottoNumbers'>
+        {numbers.map((number, index) => (
+          <Lotto key={index} value={number} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
